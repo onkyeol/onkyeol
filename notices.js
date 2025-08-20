8cde91cc6338badd64bce1c266fd21bf6ecb8432
onkyeol <ceo@onkyeol.com>
2025-08-20 17:09:50 +0900

    Add files via upload

---
+++ notices.js
@@ -0,0 +1,12 @@
+const notices = [
+  {
+    title: "홈페이지 오픈 안내",
+    date: "2025-08-15",
+    file: "posts/2025-08-15-01.html"
+  },
+  {
+    title: "서비스 업데이트 공지",
+    date: "2025-08-20",
+    file: "posts/2025-08-20-01.html"
+  }
+];
